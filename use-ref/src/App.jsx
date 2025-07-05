f1dc08261c3f1096a4678774724a75e11be823c0
anandavallis <sanandavalli7@gmail.com>
2025-07-05 14:48:38 +0530

    Create App.jsx

---
+++ use-ref/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import PreviousValueTracker from './components/PreviousValueTracker';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Previous Value Tracker</h1>
+      <PreviousValueTracker />
+    </div>
+  );
+};
+
+export default App;
